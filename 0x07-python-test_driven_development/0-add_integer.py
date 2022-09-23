0a9d030ab449148069852b2ee961b361c77d1f06
Joshua Adeoye <robertjoshua905@gmail.com>
2022-09-22 21:09:30 -0700

    first commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ This module supplies a function, add_integer(a,b) that adds two numbers"""
 
+
 def add_integer(a, b=98):
-    """Function that adds two numbers and returns the sum"""
+    """Function that adds two numbers and returns the sum
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
